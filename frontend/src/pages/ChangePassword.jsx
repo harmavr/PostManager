c715096b8c57574199bee01276dfe9bff4464930
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-05-23 13:38:16 +0300

    A simple form for old and new password inputs

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import SaveIcon from "@mui/icons-material/Save";
 
 export default function ChangePassword() {
-  return <div>ChangePassword</div>;
+  return (
+    <div>
+      <h1>Change your password</h1>
+      <input type="text" placeholder="Old password" />
+      <input type="text" name="" id="" placeholder="New password" />
+      <SaveIcon />
+    </div>
+  );
 }
