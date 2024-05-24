2c4186f88204b1295439aec2272dd9d8c4e419b9
Charis Mavrotsoupakis <harmavr@gmail.com>
2024-05-24 11:04:28 +0300

    make an end point request for updating the old password with a new one

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import SaveIcon from "@mui/icons-material/Save";
+import axios from "axios";
 
 export default function ChangePassword() {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const changePassword = () => {
+    axios
+      .put(
+        "http://localhost:3001/auth/changepassword",
+        {
+          oldPassword: oldPassword,
+          newPassword: newPassword,
+        },
+        {
+          headers: {
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.error) {
+          alert(res.error);
+        }
+      });
+  };
+
   return (
     <div>
       <h1>Change your password</h1>
-      <input type="text" placeholder="Old password" />
-      <input type="text" name="" id="" placeholder="New password" />
-      <SaveIcon />
+      <input
+        type="password"
+        placeholder="Old password"
+        onChange={(event) => {
+          setOldPassword(event.target.value);
+        }}
+      />
+      <input
+        type="password"
+        name=""
+        id=""
+        placeholder="New password"
+        onChange={(event) => {
+          setNewPassword(event.target.value);
+        }}
+      />
+      <SaveIcon onClick={changePassword} />
     </div>
   );
 }
